6f56dfe627a8dacfbd30e4f811238f7c909d4c75
ChenyuLi <lichenyu969693@gmail.com>
2014-05-30 13:48:10 -0400

    Update Map.java

--- Map.java
+++ Map.java
@@ -1,6 +1,8 @@
 public interface Map{//healing center, town, and gym will implement this
     private Tiles[][]tiles;
     public Map(){};
-    public String toString(){};
+    public int getWidth(){}
+    public int getLength(){}
+    public String toString(){}
 }
     
