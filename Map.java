5726c51e8617a86e6c216d2bae4984fa75775d9a
ChenyuLi <lichenyu969693@gmail.com>
2014-06-03 13:42:27 -0400

    Update Map.java

--- Map.java
+++ Map.java
@@ -10,8 +10,16 @@ public class Map{//healing center, town, and gym will implement this
 	return tiles[0].length;
     }
     public String toString(){
-	return "a";
+	String string = "";
+	for (row = 0; row < a.length; row++){
+	    string = string + tiles[row][0];
+	        for (column = 1; column < tiles[row].length; column++) {
+                    string = string + " " + tiles[row][column];
+                }
+            }
+            string = string + "\n";
+        }
+	return string;	
     }
-
 }
     
